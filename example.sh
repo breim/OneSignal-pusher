af39cd76a41c12217278c9385b5a19462a05de38
Henrique Breim <breim@protonmail.ch>
2020-09-01 19:22:47 -0300

    Update example.sh

--- example.sh
+++ example.sh
@@ -3,7 +3,7 @@ curl --location --request POST 'https://onesignal.com/api/v1/notifications' \
 --header 'Authorization: Basic PUT HERE YOUR KEY' \
 --data-raw '{
   "app_id": "8d86cc23-d5ac-45d6-84ee-3c76021b2c0e",
-  "include_external_user_ids": ["909291"],
+  "include_external_user_ids": ["909292"],
   "data": {"foo": "bar"},
   "contents": {"en": "Hello! Friend"}
 }
